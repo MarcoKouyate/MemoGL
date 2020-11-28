3a400512a1b5dfaf135feb382596a15935d5c363
Marco Kouyaté <marco.kouyate@yahoo.fr>
2020-11-28 17:51:48 +0100

    Put external libraries in third-party IDE folder

--- cmake/macros.cmake
+++ cmake/macros.cmake
@@ -1,7 +1,23 @@
-#==== MACRO
+#==== Macro for setting a test executable
 macro(package_add_test TESTNAME FILES LIBRARIES TEST_WORKING_DIRECTORY)
     add_executable(${TESTNAME} ${FILES})
     target_link_libraries(${TESTNAME} gtest gmock gtest_main ${LIBRARIES})
     set_target_properties(${TESTNAME} PROPERTIES FOLDER tests)
     add_test(${TESTNAME} ${TESTNAME})
 endmacro()
+
+
+#==== Add subtargets inside a IDE folder
+set_property(GLOBAL PROPERTY USE_FOLDERS TRUE)
+define_property(
+    TARGET
+    PROPERTY FOLDER
+    INHERITED
+    BRIEF_DOCS "Set the folder name."
+    FULL_DOCS  "Use to organize targets in an IDE."
+)
+
+function(add_subdirectory_with_folder _folder_name _folder)
+    add_subdirectory(${_folder} ${ARGN})
+    set_property(DIRECTORY "${_folder}" PROPERTY FOLDER "${_folder_name}")
+endfunction()
