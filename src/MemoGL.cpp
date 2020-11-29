b7dea367ca0f9900e18d82c84626d08bf160dd4d
Marco Kouyaté <marco.kouyate@yahoo.fr>
2020-11-29 03:21:01 +0100

    Fix linking issues with glfw (MVSC)

--- src/MemoGL.cpp
+++ src/MemoGL.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+
+
 namespace MemoGL{
     int main(int argc, char** argv) {
+        glfwInit();
+        glewInit();
         std::cout << "MemoGL.cpp" << std::endl;
         return 0;
     }
