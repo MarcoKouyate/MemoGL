e278df066651bc615b789ee9f4bae2c580039b0c
Marco Kouyaté <marco.kouyate@yahoo.fr>
2021-01-17 23:11:41 +0100

    Update Windows window creation

--- src/platform/windows/WindowsContext.cpp
+++ src/platform/windows/WindowsContext.cpp
@@ -1,7 +1,13 @@
 #include "graphics/context/GLFW/GLFWContext.h"
 
 namespace MemoGL {
-    IContext* IContext::create(const WindowSettings& properties) {
-        return new GLFWContext(properties);
+    IContext* IContext::create(const WindowSettings& windowSettings) {
+        RenderingAPISettings renderingSettings;
+        renderingSettings.forward_compatibility = true;
+        renderingSettings.version_major = 4;
+        renderingSettings.version_minor = 4;
+        renderingSettings.profile = APIProfile::Core;
+
+        return new GLFWContext(windowSettings, renderingSettings);
     }
 }
